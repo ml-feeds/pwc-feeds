28851f2e9a52a0c0b38dce26d28c870b11df267f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-01 17:20:20 -0500

    Check path

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from typing import Dict, Tuple
 
 import flask
 
+from pwc import config
 from pwc.feed import Feed
 
 log = logging.getLogger(__name__)
 feed = Feed()
+
+VALID_PATHS = {f'/{feed_type}' for feed_type in config.FEED_TYPES}
+VALID_PATHS_STR = ', '.join(VALID_PATHS)
 
 
 def serve(request: flask.Request) -> Tuple[bytes, int, Dict[str, str]]:
     hget = request.headers.get
     log.info('Received request for path %s from %s from %s, %s, %s.', request.path, hget('X-Appengine-User-Ip'),
              hget('X-Appengine-City'), hget('X-Appengine-Region'), hget('X-Appengine-Country'))
-    if request.path is None:
-        raise flask.abort(404)
-    path = request.path[1:]  # Strip /
-    return feed.feed(path), 200, {'Content-Type': 'text/xml; charset=utf-8'}
+
+    if request.path not in VALID_PATHS:
+        request_path = '' if request.path is None else request.path
+        return f'Request path "{request_path}" is invalid. Use one of: {VALID_PATHS_STR}', 400
+
+    feed_type = request.path[1:]  # Strip "/"
+    return feed.feed(feed_type), 200, {'Content-Type': 'text/xml; charset=utf-8'}
