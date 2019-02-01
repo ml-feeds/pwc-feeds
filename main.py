4c5935e2a35c15021762c72c5bcdd41eb508c261
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-01 13:20:14 -0500

    Add "serve" callable, also using request.path

---
+++ main.py
@@ -0,0 +1,16 @@
+import logging
+from typing import Dict, Tuple
+
+import flask
+
+from pwc.feed import Feed
+
+log = logging.getLogger(__name__)
+feed = Feed()
+
+
+def serve(request: flask.Request) -> Tuple[bytes, int, Dict[str, str]]:
+    hget = request.headers.get
+    log.info('Received request for path %s from %s from %s, %s, %s.', request.path, hget('X-Appengine-User-Ip'),
+             hget('X-Appengine-City'), hget('X-Appengine-Region'), hget('X-Appengine-Country'))
+    return feed.feed(request.path), 200, {'Content-Type': 'text/xml; charset=utf-8'}
