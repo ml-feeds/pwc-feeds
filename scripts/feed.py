56a867bcd46b38482bfb78967a4dfb55480ca692
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-01 18:46:42 -0500

    Simplify

--- scripts/feed.py
+++ scripts/feed.py
@@ -1,23 +1,19 @@
 import logging
 import time
 
-from pwc import config
 from pwc.feed import Feed
 
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    feed = Feed()
-    feed_types = list(config.FEED_TYPES)[:1]
+    feed = Feed('trending')
     try:
-        for feed_type in feed_types:
-            output = feed.feed(feed_type)
-            print(output.decode())
+        output = feed.feed()
+        print(output.decode())
 
-        for feed_type in feed_types:
-            log.info('Testing cachetools cache.')
-            assert feed.feed(feed_type) == output
-            log.info('Tested cachetools cache.')
+        log.info('Testing cachetools cache.')
+        assert feed.feed() == output
+        log.info('Tested cachetools cache.')
     except Exception:
         time.sleep(.01)  # Delay for logs to flush.
         raise
