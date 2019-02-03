c659ee5d63c751dc3f593c5f91a05f22715f38f2
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-03 17:41:57 -0500

    Reinit feed when output is requested

--- pwc/feed.py
+++ pwc/feed.py
@@ -22,21 +22,21 @@ class Feed:
         self._html_request = Request(html_url, headers={'User-Agent': config.USER_AGENT})
 
         self._feed_type = feed_type
-        self._feed_desc = f'for "{self._feed_type}"'
-        self._feed = FeedGenerator()
+        self._feed_type_desc = f'for "{self._feed_type}"'
+        self._feed_title = config.FEED_TITLE_TEMPLATE.format(feed_type=self._feed_type.title())
+
         self._hext_rule_extract = Rule(config.HTML_HEXT).extract
         self._is_debug_logged = log.isEnabledFor(logging.DEBUG)
-        self._init_feed()
 
         self._output = lru_cache(maxsize=1)(self._output)  # type: ignore  # Instance level cache
         self.feed = ttl_cache(maxsize=1, ttl=config.CACHE_TTL)(self.feed)  # type: ignore  # Instance level cache
 
     def _output(self, text: bytes) -> bytes:  # type: ignore
-        feed_desc = self._feed_desc
+        feed_type_desc = self._feed_type_desc
         items = self._hext_rule_extract(Html(text.decode()))
-        log.info('HTML input %s has %s items.', feed_desc, len(items))
+        log.info('HTML input %s has %s items.', feed_type_desc, len(items))
 
-        feed = self._feed
+        feed = self._init_feed()
         is_debug_logged = self._is_debug_logged
         for item in items:
             if 'categories' not in item:
@@ -56,23 +56,23 @@ class Feed:
                 log.debug('Added: %s', item['title'])
 
         text_: bytes = feed.rss_str(pretty=True)
-        log.info('XML output %s has %s items.', feed_desc, text_.count(b'<item>'))
+        log.info('XML output %s has %s items.', feed_type_desc, text_.count(b'<item>'))
         return text_
 
-    def _init_feed(self) -> None:
-        feed = self._feed
-        title = config.FEED_TITLE_TEMPLATE.format(feed_type=self._feed_type.title())
-        feed.title(title)
+    def _init_feed(self) -> FeedGenerator:
+        feed = FeedGenerator()
+        feed.title(self._feed_title)
         feed.link(href=config.REPO_URL, rel='self')
         feed.description(config.FEED_DESCRIPTION)
+        return feed
 
     def feed(self) -> bytes:  # type: ignore
-        feed_desc = self._feed_desc
-        log.debug(f'Reading HTML %s.', feed_desc)
+        feed_type_desc = self._feed_type_desc
+        log.debug(f'Reading HTML %s.', feed_type_desc)
         text = urlopen(self._html_request).read()
-        log.info('HTML input %s has size %s.', feed_desc, humanize_len(text))
+        log.info('HTML input %s has size %s.', feed_type_desc, humanize_len(text))
         text = self._output(text)
-        log.info('XML output %s has size %s.', feed_desc, humanize_len(text))
+        log.info('XML output %s has size %s.', feed_type_desc, humanize_len(text))
         return text
 
 
